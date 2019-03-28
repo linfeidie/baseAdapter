3bdb121a1500f4e9dfed741a214e33cb4e280055
linfeidie <494520748@qq.com>
2019-03-28 12:22:02 +0800

    测试提交

--- sample/src/main/java/com/zhy/sample/DividerItemDecoration.java
+++ sample/src/main/java/com/zhy/sample/DividerItemDecoration.java
@@ -26,6 +26,7 @@ public class DividerItemDecoration extends RecyclerView.ItemDecoration {
             android.R.attr.listDivider
     };
 
+
     public static final int HORIZONTAL_LIST = LinearLayoutManager.HORIZONTAL;
 
     public static final int VERTICAL_LIST = LinearLayoutManager.VERTICAL;
